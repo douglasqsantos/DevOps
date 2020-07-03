39e8196487c588ffb2c230205fc07025e80aa1c8
Douglas Quintiliano dos Santos <douglas.q.santos@gmail.com>
2020-07-02 21:42:46 -0300

    Adding Monokai Theme

--- Misc/prep-vim.sh
+++ Misc/prep-vim.sh
@@ -2,6 +2,7 @@
 
 VIMRC="https://raw.githubusercontent.com/douglasqsantos/DevOps/master/Misc/vimrc"
 GRUVBOX="https://raw.githubusercontent.com/douglasqsantos/DevOps/master/Misc/gruvbox.vim"
+MONOKAI="https://raw.githubusercontent.com/douglasqsantos/DevOps/master/Misc/monokai.vim"
 
 
 [ ! -d "~/.vim/colors" ] && mkdir -p ~/.vim/colors
@@ -11,3 +12,6 @@ curl -L ${VIMRC} -o ~/.vimrc
 
 [ -f "~/.vim/colors/gruvbox.vim" ] && mv ~/.vim/colors/gruvbox.vim ~/.vim/colors/gruvbox.vim.bkp
 curl -L ${GRUVBOX} -o ~/.vim/colors/gruvbox.vim
+
+[ -f "~/.vim/colors/monokai.vim" ] && mv ~/.vim/colors/monokai.vim ~/.vim/colors/monokai.vim.bkp
+curl -L ${MONOKAI} -o ~/.vim/colors/monokai.vim
